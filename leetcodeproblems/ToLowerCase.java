25b60e7725e0e201a67229fbdcdc710b908162c5
RITAM DE <ritamdey.1998@gmail.com>
2025-01-28 01:03:53 +0530

    https://leetcode.com/problems/to-lower-case/description/

---
+++ leetcodeproblems/ToLowerCase.java
@@ -0,0 +1,30 @@
+package leetcodeproblems;
+
+import java.util.Locale;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello"));
+    }
+    /**
+     * @LeetCode
+     * @https://leetcode.com/problems/to-lower-case/description/
+     * Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+     *
+     * Example 1:
+     *
+     * Input: s = "Hello"
+     * Output: "hello"
+     * Example 2:
+     *
+     * Input: s = "here"
+     * Output: "here"
+     * Example 3:
+     *
+     * Input: s = "LOVELY"
+     * Output: "lovely"
+     * */
+    public static String toLowerCase(String s) {
+        return s.toLowerCase(Locale.ROOT);
+    }
+}
