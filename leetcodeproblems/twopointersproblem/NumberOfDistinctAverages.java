cf1930586a6b6092560ba4a35d534af0b2fc94c4
RITAM DE <ritamdey.1998@gmail.com>
2025-04-20 20:55:10 +0530

    @2465. Number of Distinct Averages, @Easy, @https://leetcode.com/problems/number-of-distinct-averages/description/?envType=problem-list-v2&envId=two-pointers

---
+++ leetcodeproblems/twopointersproblem/NumberOfDistinctAverages.java
@@ -0,0 +1,62 @@
+package leetcodeproblems.twopointersproblem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NumberOfDistinctAverages {
+    public static void main(String[] args) {
+        System.out.println(distinctAverages(new int[]{1,100}));
+    }
+    /**
+     * @Leetcode
+     * @2465. Number of Distinct Averages
+     * @https://leetcode.com/problems/number-of-distinct-averages/description/?envType=problem-list-v2&envId=two-pointers
+     *
+     * You are given a 0-indexed integer array nums of even length.
+     *
+     * As long as nums is not empty, you must repetitively:
+     *
+     * Find the minimum number in nums and remove it.
+     * Find the maximum number in nums and remove it.
+     * Calculate the average of the two removed numbers.
+     * The average of two numbers a and b is (a + b) / 2.
+     *
+     * For example, the average of 2 and 3 is (2 + 3) / 2 = 2.5.
+     * Return the number of distinct averages calculated using the above process.
+     *
+     * Note that when there is a tie for a minimum or maximum number, any can be removed.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: nums = [4,1,4,0,3,5]
+     * Output: 2
+     * Explanation:
+     * 1. Remove 0 and 5, and the average is (0 + 5) / 2 = 2.5. Now, nums = [4,1,4,3].
+     * 2. Remove 1 and 4. The average is (1 + 4) / 2 = 2.5, and nums = [4,3].
+     * 3. Remove 3 and 4, and the average is (3 + 4) / 2 = 3.5.
+     * Since there are 2 distinct numbers among 2.5, 2.5, and 3.5, we return 2.
+     * Example 2:
+     *
+     * Input: nums = [1,100]
+     * Output: 1
+     * Explanation:
+     * There is only one average to be calculated after removing 1 and 100, so we return 1.
+     * */
+    public static int distinctAverages(int[] nums) {
+        Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length - 1;
+        List<Double> list = new ArrayList<>();
+        while(start <  end){
+            double average = ((double) (nums[start] + nums[end]) / 2);
+            list.add(average);
+            start++;
+            end --;
+        }
+        return (int) list.stream().distinct().count();
+    }
+}
