eddcfc126048f24f674bdc50d244dc8fc04386d2
RITAM DE <ritamdey.1998@gmail.com>
2025-08-14 02:01:42 +0530

    @326. Power of Three, @Easy, @https://leetcode.com/problems/power-of-three/description/?envType=daily-question&envId=2025-08-13

---
+++ leetcodeproblems/dailyleetcodeproblems/PowerOfThree.java
@@ -0,0 +1,46 @@
+package leetcodeproblems.dailyleetcodeproblems;
+
+public class PowerOfThree {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(27));
+    }
+    /**
+     * @LeetCode
+     * @326. Power of Three
+     * @https://leetcode.com/problems/power-of-three/description/?envType=daily-question&envId=2025-08-13
+     *
+     * Given an integer n, return true if it is a power of three. Otherwise, return false.
+     *
+     * An integer n is a power of three, if there exists an integer x such that n == 3x.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: n = 27
+     * Output: true
+     * Explanation: 27 = 33
+     * Example 2:
+     *
+     * Input: n = 0
+     * Output: false
+     * Explanation: There is no x where 3x = 0.
+     * Example 3:
+     *
+     * Input: n = -1
+     * Output: false
+     * Explanation: There is no x where 3x = (-1).
+     *
+     *
+     * Constraints:
+     *
+     * -231 <= n <= 231 - 1
+     *
+     * */
+    public static boolean isPowerOfThree(int n) {
+
+        if(n <= 0) return false;
+        while (n%3 == 0) n/=3;
+        return n == 1;
+    }
+}
